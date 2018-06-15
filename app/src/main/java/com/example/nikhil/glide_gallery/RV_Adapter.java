9c836095470c94aec837ab4c08f93f4d97634b72
nikhil dhyani <nikhildhyani365@gmail.com>
2018-06-15 16:06:32 +0530

    Glide Added | Working

--- app/src/main/java/com/example/nikhil/glide_gallery/RV_Adapter.java
+++ app/src/main/java/com/example/nikhil/glide_gallery/RV_Adapter.java
@@ -5,7 +5,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import java.util.List;
 
@@ -42,6 +46,14 @@ public class RV_Adapter extends RecyclerView.Adapter<RV_Adapter.myViewHolder> {
         Photo listPhotos = mphotos.get(position);
         holder.tv.setText(listPhotos.getName());
         holder.url.setText(listPhotos.getUrl().getSmall());
+        //String urll = holder.url.getText(listPhotos.getUrl().getSmall());
+        String urll = listPhotos.getUrl().getLarge();
+        Glide.with(mcontext).load(urll)
+                .thumbnail(0.5f)
+                .crossFade()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(holder.img);
+
 
     }
 
@@ -53,7 +65,7 @@ public class RV_Adapter extends RecyclerView.Adapter<RV_Adapter.myViewHolder> {
     public class myViewHolder extends RecyclerView.ViewHolder
     {
         TextView tv,url;
-
+        ImageView img;
 
         public myViewHolder(View itemView) {
             super(itemView);
@@ -61,6 +73,7 @@ public class RV_Adapter extends RecyclerView.Adapter<RV_Adapter.myViewHolder> {
 
              tv = itemView.findViewById(R.id.ctv_id);
              url = itemView.findViewById(R.id.c_tv_url);
+             img = itemView.findViewById(R.id.c_img_id);
         }
     }
 }
